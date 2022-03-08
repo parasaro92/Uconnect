2f1ea0518fe76ba16a35c3479cca9424238318ba
Veerendra Makena <veerendra.makena@gmail.com>
2022-03-07 20:23:11 -0700

    Updated mongo db config

--- SERVER/routes/auth.js
+++ SERVER/routes/auth.js
@@ -1,63 +1,72 @@
 const express = require('express')
-const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv').config();
+const requireLogin = require('../middleware/requireLogin')
+const router = express.Router()
 
-router.get('./', (req, res) => {
-    res.send("hello")
+router.get('/protected',requireLogin,(req,res)=>{
+        res.send('hello user')
+
 })
 
-router.post('/signup', (req, res) => {
-    const { name, email, password } = req.body
-    if (!email || !password || !name) {
-        return res.status(422).json({ error: "please add all the fields" })
+router.post('/Signup',(req,res)=>{
+    const {name,email,password} = req.body
+    if(!email ||!password||!name){
+        return res.status(422).json({Error:' Please add all the fields'})
     }
-    User.findOne({ email: email })
-        .then((savedUser) => {
-            if (savedUser) {
-                return res.status(422).json({ error: "user already exists with that email" })
-            }
-            bcrypt.hash(password, 12)
-                .then(hashespassword => {
-                    const user = new User({
-                        email,
-                        password,
-                        name
-                    })
-                    user.save()
-                        .then(user => {
-                            res.json({ message: "Saved Succesfully" })
-                        })
-                        .catch(err => {
-                            console.log(err)
-                        })
+    User.findOne({email:email})
+    .then((savedUser)=>{
+        if(savedUser){
+            return res.status(422).json({error:'user already exists with that email'})
+        }
+        bcrypt.hash(password,12)
+        .then(hashedpassword=>{
 
-                })
 
+        const user = new User({
+            name,
+            email,
+            password:hashedpassword
+           
         })
-        .catch(err => {
+
+        user.save()
+        .then(user=>{
+            res.json({message:'Saved Successfully'})
+        })
+        .catch(err=>{
             console.log(err)
         })
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
 })
 
-router.post('./signin',(req,res)=>{
+router.post('/Signin',(req,res)=>{
     const{email,password} = req.body
     if(!email || !password){
-      return  res.status(422).json({error:"please add email or password"})
+       return res.status(422).json({error:'Please add email or password'})
     }
     User.findOne({email:email})
     .then(savedUser=>{
         if(!savedUser){
-          return  res.status(422).json({error:"Invalid Email or password"})
+           return res.status(422).json({error:'Invalid email or password'})
         }
         bcrypt.compare(password,savedUser.password)
         .then(doMatch=>{
             if(doMatch){
-                res.json({message:"succesfully signed in"})
+                //res.json({message:'Signed in Successfully'})
+                const token = jwt.sign({_id:savedUser._id},process.env.JWT_SECRET)
+                res.json({token})
             }
             else{
-                return  res.status(422).json({error:"Invalid Email or password"})
+                return res.status(422).json({error:'Invalid email or password'})
+                
             }
         })
         .catch(err=>{
@@ -65,4 +74,5 @@ router.post('./signin',(req,res)=>{
         })
     })
 })
+
 module.exports = router
--- Uconnect/src/Components/pages/Profile.js
+++ Uconnect/src/Components/pages/Profile.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React from 'react'
 
-const Profile = () => {
-    return (
-        <h1>Profile</h1>
-    )
-}
+const Profile = () =>{
+    return(
+        <div style={{maxWidth:'900px',margin:'0px auto'}}>
+            <div style={{
+                display:'flex',
+                justifyContent:'space-around',
+                margin:'20px 0px',
+                maxWidth:'600px'
+            }}>
+                    <div style={{fontFamily:'Calibri',marginLeft:'50px'}}>
+                    <img style={{width:'160px',height:'160px',borderRadius:'80px'}}
+                    src='https://www.dailycameranews.com/wp-content/uploads/2015/06/Sony-RX100-IV-Sample-Images.jpg'
+                   />
+                   </div>
+                    <div style={{fontFamily:'Calibri',marginTop:'20px'}} >
+                        <h2>Veerendra Makena</h2>
+                    
+                        <div style={{display:'flex',justifyContent:'space-between',width:'108%'}}>
+                        <h4>10 Posts</h4>
+                        <h4>10 Followers</h4>
+                        <h4>10 Following</h4>
+                        </div>
+                    </div>
+                    
+                
+            </div>
+            <div className='gallery'>
+            <img className='item' src='https://cdn.pixabay.com/photo/2020/06/14/18/11/the-caucasus-5298803_960_720.jpg'/>
+            <img className='item' src='https://cdn.pixabay.com/photo/2022/01/31/15/18/coffee-6984075_960_720.jpg'/>
+            <img className='item' src='https://cdn.pixabay.com/photo/2020/06/14/18/11/the-caucasus-5298803_960_720.jpg'/>
+            <img className='item' src='https://cdn.pixabay.com/photo/2022/01/31/15/18/coffee-6984075_960_720.jpg'/>
+            <img className='item' src='https://cdn.pixabay.com/photo/2020/06/14/18/11/the-caucasus-5298803_960_720.jpg'/>
+            <img className='item' src='https://cdn.pixabay.com/photo/2022/01/31/15/18/coffee-6984075_960_720.jpg'/>
+            </div>
+            </div>
 
-export default Profile
+    );
+};
+
+export default Profile;
